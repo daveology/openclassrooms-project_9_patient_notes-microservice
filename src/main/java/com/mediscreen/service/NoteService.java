8c577d2e2b289947464b03e6102d454c0a649ccd
David <cheery.spot1750@fastmail.com>
2021-12-30 22:04:14 +0100

    build: implement addNote into service

--- src/main/java/com/mediscreen/service/NoteService.java
+++ src/main/java/com/mediscreen/service/NoteService.java
@@ -1,5 +1,6 @@
 package com.mediscreen.service;
 
+import com.mediscreen.model.Note;
 import com.mediscreen.repository.NoteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,4 +10,9 @@ public class NoteService {
 
     @Autowired
     private NoteRepository noteRepository;
+
+    public void addNote(Note note) {
+
+        noteRepository.insert(note);
+    }
 }
