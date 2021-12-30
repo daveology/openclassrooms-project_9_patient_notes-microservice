e26befed68cbb55fa44088347fc4d455f0755ee5
David <cheery.spot1750@fastmail.com>
2021-12-30 22:03:00 +0100

    build: instanciate NoteRepository into service

--- src/main/java/com/mediscreen/service/NoteService.java
+++ src/main/java/com/mediscreen/service/NoteService.java
@@ -1,9 +1,12 @@
 package com.mediscreen.service;
 
+import com.mediscreen.repository.NoteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class NoteService {
 
-
+    @Autowired
+    private NoteRepository noteRepository;
 }
