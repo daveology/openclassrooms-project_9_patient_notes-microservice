74fb8c26b91515ed71064c357c4546f6e65c5631
David <cheery.spot1750@fastmail.com>
2021-12-30 21:53:22 +0100

    build: update Note model

--- src/main/java/com/mediscreen/model/Note.java
+++ src/main/java/com/mediscreen/model/Note.java
@@ -2,8 +2,42 @@ package com.mediscreen.model;
 
 import org.springframework.data.mongodb.core.mapping.Document;
 
+import java.util.Objects;
+
 @Document
 public class Note {
 
+    private Long patientId;
+    private String content;
 
+    public Long getPatientId() {
+        return patientId;
+    }
+
+    public void setPatientId(Long patientId) {
+        this.patientId = patientId;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Note(){}
+
+    public Note(Long patientId, String content) {
+        this.patientId = patientId;
+        this.content = content;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Note note = (Note) o;
+        return Objects.equals(patientId, note.patientId) && Objects.equals(content, note.content);
+    }
 }
