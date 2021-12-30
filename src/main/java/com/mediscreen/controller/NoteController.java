c02b2ccfb7f253dc9677ca7abd2aa748a02694e1
David <cheery.spot1750@fastmail.com>
2021-12-30 22:14:59 +0100

    build: add getNotesByPatientId endpoint

--- src/main/java/com/mediscreen/controller/NoteController.java
+++ src/main/java/com/mediscreen/controller/NoteController.java
@@ -5,10 +5,9 @@ import com.mediscreen.service.NoteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
 
 @RestController
 @RequestMapping("/note")
@@ -22,5 +21,11 @@ public class NoteController {
 
         noteService.addNote(note);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping
+    public ResponseEntity<Collection<Note>> getNotesByPatientId() {
+
+        return ResponseEntity.ok(noteService.getNotesByPatientId());
     }
 }
