74acc54f91e5fb538c4431f51ce0442a43713d8e
David <cheery.spot1750@fastmail.com>
2021-12-30 22:05:52 +0100

    build: implement addNote into controller

--- src/main/java/com/mediscreen/controller/NoteController.java
+++ src/main/java/com/mediscreen/controller/NoteController.java
@@ -1,5 +1,9 @@
 package com.mediscreen.controller;
 
+import com.mediscreen.model.Note;
+import com.mediscreen.service.NoteService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,8 +11,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/note")
 public class NoteController {
 
-    public void addNote() {
+    @Autowired
+    NoteService noteService;
 
+    public void addNote(@RequestBody Note note) {
 
+        noteService.addNote(note);
     }
 }
